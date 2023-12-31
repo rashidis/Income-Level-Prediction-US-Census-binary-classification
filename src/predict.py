3cdb5ab035b2350c5ef5f825385947a5e10f173f
SHIMA RASHIDI <shima.rashidi7@gmail.com>
2023-12-31 16:56:39 +1100

    explainability added

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,9 @@
 import json
 import pandas as pd
 import pickle
+import shap
+import matplotlib.pyplot as plt
+import matplotlib
 from sklearn.metrics import classification_report
 from preprocessing import preprocessing
 from preprocessing import encoding
@@ -31,8 +34,11 @@ if __name__ == "__main__":
     print(f'shape of validation dataset is {df_val.shape}')
     
     # Setup validation data
-    X = df_val.drop(columns=["income"])
-    y = df_val["income"]
+    if 'income' in df_val.columns:  # if the dataset is a validation dataset
+        X = df_val.drop(columns=["income"])
+        y = df_val["income"]
+    else:
+        X = df_val
 
     # Load model
     with open('../results/model.pkl', 'rb') as file:
@@ -40,12 +46,29 @@ if __name__ == "__main__":
         
     # Predict
     y_pred_val = model.predict(X)
-    print("\nClassification Report:\n", classification_report(y, y_pred_val))
+        
+    # Exaplainability
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+
+    waterfall = shap.summary_plot(shap_values, X, feature_names=X.columns, show=False)
+    plt.savefig('../results/validation_waterfall.png')
+
+    class_index = 0 #below 50k
+    data_index = 0
+    force_plot = shap.force_plot(explainer.expected_value[class_index], 
+                shap_values[class_index][data_index, :], X.iloc[data_index, :], 
+                matplotlib=matplotlib, show=False)
+    plt.savefig(f'../results/val_force_plot_class{class_index}_index{data_index}.png')
     
     # Save results
-    X['income'] = y
+    if 'incom' in df_val.columns:  # if the dataset is a validation dataset
+        print("\nClassification Report:\n", classification_report(y, y_pred_val))
+        with open('../results/val_classification_report.txt', 'w') as output_file:
+            output_file.write(str(classification_report(y, y_pred_val)))
+        X['income'] = y
+        
     X['preds'] = y_pred_val
-    X.to_csv('../results/validation_preds.csv', index=False)
+    X.to_csv('../results/prediction_results.csv', index=False)
     
-    with open('../results/val_classification_report.txt', 'w') as output_file:
-        output_file.write(str(classification_report(y, y_pred_val)))
+
